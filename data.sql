8002d892cc416aa58b2c124c329a327f2678a146
skygroundwater <o.metelev2020@yande.ru>
2023-12-21 07:51:25 +0300

    changes

---
+++ data.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.organizations VALUES ('e6a625cc-718b-48c2-ac76-1dfdff9a531e', 'Ostock', 'Illary Huaylupo', 'illaryhs@gmail.com', 'e6a625cc-718b-48c2-ac76-1dfdff9a531e');
+INSERT INTO public.organizations VALUES ('d898a142-de44-466c-8c88-9ceb2c2429d3', 'OptimaGrowth', 'Admin', 'illaryhs@gmail.com', 'd898a142-de44-466c-8c88-9ceb2c2429d3');
+INSERT INTO public.organizations VALUES ('e839ee96-28de-4f67-bb79-870ca89743a0', 'Ostock', 'Illary Huaylupo', 'illaryhs@gmail.com', 'e839ee96-28de-4f67-bb79-870ca89743a0');
--- src/main/java/com/optimagrowth/organizationservice/controller/OrganizationController.java
+++ src/main/java/com/optimagrowth/organizationservice/controller/OrganizationController.java
@@ -3,9 +3,12 @@ package com.optimagrowth.organizationservice.controller;
 import com.optimagrowth.organizationservice.model.Organization;
 import com.optimagrowth.organizationservice.service.OrganizationService;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Role;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.security.RolesAllowed;
 
 @RestController
 @RequestMapping(value = "v1/organization")
@@ -16,23 +19,27 @@ public class OrganizationController {
 
 
     @RequestMapping(value = "/{organizationId}", method = RequestMethod.GET)
+    @RolesAllowed({"ADMIN","USER"})
     public ResponseEntity<Organization> getOrganization(@PathVariable("organizationId") String organizationId) {
         return ResponseEntity.ok(service.findById(organizationId));
     }
 
     @RequestMapping(value = "/{organizationId}", method = RequestMethod.PUT)
+    @RolesAllowed({"ADMIN","USER"})
     public void updateOrganization(@PathVariable("organizationId") String id,
                                    @RequestBody Organization organization) {
         service.update(organization);
     }
 
     @PostMapping
+    @RolesAllowed({"ADMIN","USER"})
     public ResponseEntity<Organization> saveOrganization(@RequestBody Organization organization) {
         return ResponseEntity.ok(service.create(organization));
     }
 
     @RequestMapping(value = "/{organizationId}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RolesAllowed({"ADMIN"})
     public void deleteOrganization(@PathVariable("organizationId") String id,
                                    @RequestBody Organization organization) {
         service.delete(organization);
